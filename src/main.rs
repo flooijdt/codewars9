8603bfe717eaaf8afea4f2463ed360f09ed31498
flooijdt <flooijdt@posteo.de>
2023-02-18 09:59:26 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
 fn to_camel_case(text: &str) -> String {
     let mut vecky: Vec<char> = text.chars().collect();
     let mut index = 0;
-    for i in vecky.clone().iter().enumerate() {
+    for i in vecky.clone().iter_mut().enumerate() {
         if i.1 == &'-' || i.1 == &'_' {
             vecky[i.0 + 1] = vecky[i.0 + 1].to_ascii_uppercase();
             vecky.remove(i.0);
+            i.0 -= 1;
         }
-        index += 1;
     }
     vecky.into_iter().collect::<String>()
 }
